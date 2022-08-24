f66d2cf49fb60032b7c7cbc491a7de62ece74b5a
unknown <govindkmr141@gmail.com>
2022-08-24 12:58:09 +0530

    project file initial commit

---
+++ uboric-clone/src/App.js
@@ -0,0 +1,12 @@
+import { Heading } from "@chakra-ui/react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Heading as="h1">Here comes the sun</Heading>
+    </div>
+  );
+}
+
+export default App;
